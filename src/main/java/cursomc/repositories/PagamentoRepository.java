85b026e13cb7d5f0226a684a619036c716121334
Keilane Rizza <keilanerizza@gmail.com>
2018-08-30 22:38:27 -0300

    Pedido, EstadoPagamento e Pagamento

---
+++ src/main/java/cursomc/repositories/PagamentoRepository.java
@@ -0,0 +1,11 @@
+package cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import cursomc.domain.Pagamento;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento, Integer> {
+
+}
---
+++ src/main/java/cursomc/repositories/PedidoRepository.java
@@ -0,0 +1,11 @@
+package cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import cursomc.domain.Pedido;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+
+}
