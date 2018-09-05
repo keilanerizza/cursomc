5baaf273c847fdac25fd7022fb48a9971fd0dbf8
Keilane Rizza <keilanerizza@gmail.com>
2018-09-04 22:06:26 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/cursomc/CursomcApplication.java
+++ src/main/java/cursomc/CursomcApplication.java
@@ -13,6 +13,7 @@ import cursomc.domain.Cidade;
 import cursomc.domain.Cliente;
 import cursomc.domain.Endereco;
 import cursomc.domain.Estado;
+import cursomc.domain.ItemPedido;
 import cursomc.domain.Pagamento;
 import cursomc.domain.PagamentoComBoleto;
 import cursomc.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import cursomc.repositories.CidadeRepository;
 import cursomc.repositories.ClienteRepository;
 import cursomc.repositories.EnderecoRepository;
 import cursomc.repositories.EstadoRepository;
+import cursomc.repositories.ItemPedidoRepository;
 import cursomc.repositories.PagamentoRepository;
 import cursomc.repositories.PedidoRepository;
 import cursomc.repositories.ProdutoRepository;
@@ -48,6 +50,8 @@ public class CursomcApplication implements CommandLineRunner {
 	private PedidoRepository pedidoRepository;
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -112,6 +116,19 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+		
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+		
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
 		
 	}
 }
---
+++ src/main/java/cursomc/repositories/ItemPedidoRepository.java
@@ -0,0 +1,11 @@
+package cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import cursomc.domain.ItemPedido;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer> {
+
+}
