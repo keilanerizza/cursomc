57ca9cd633e8a4bba3a463d98a83db11238ddb2c
Keilane Rizza <keilanerizza@gmail.com>
2018-11-06 17:26:58 -0200

    Inserindo Pedido

--- src/main/java/cursomc/domain/PagamentoComCartao.java
+++ src/main/java/cursomc/domain/PagamentoComCartao.java
@@ -2,9 +2,12 @@ package cursomc.domain;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
+
 import cursomc.domain.enums.EstadoPagamento;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento{
 	private static final long serialVersionUID = 1L;
 
